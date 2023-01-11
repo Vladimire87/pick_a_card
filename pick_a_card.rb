a6d6811cdbfbbb5ea74d3dfc9e80e14cfaa93bca
Vladimire87 <v.egorov@bslimousine.com>
2023-01-11 16:11:30 +0400

    initial commit

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+suit = %w[hearts diamonds clubs spades]
+cards = %w[ace two three four five six seven eight nine ten jack queen king]
+puts "Вы выкинули #{suit.sample} #{cards.sample}"
